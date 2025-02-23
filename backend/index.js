029444846d9690c6fff45342c3f8542d812803d5
Noella <Noellaikirezi@gmail.com>
2025-02-23 21:03:00 +0100

    Ajouter une route d'interaction blockchain pour vérifier le solde

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,18 @@
+const Web3 = require('web3');
 const express = require('express');
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello from the backend!');
+const web3 = new Web3('https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID');
+
+app.get('/balance/:address', async (req, res) => {
+  const address = req.params.address;
+  try {
+    const balance = await web3.eth.getBalance(address);
+    res.send({ address, balance: web3.utils.fromWei(balance, 'ether') });
+  } catch (error) {
+    res.status(400).send({ error: 'Invalid address' });
+  }
 });
 
 app.listen(port, () => {
